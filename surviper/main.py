4021b56f71b48f948c6c1022f6a7ed2b26497f30
Dario Mavec <dario.mavec@gmail.com>
2022-07-25 16:58:18 +1000

    Setting up facenet

--- surviper/main.py
+++ surviper/main.py
@@ -1,1 +1,20 @@
-print(1)
+from keras_facenet import FaceNet
+from PIL import Image
+from numpy import asarray
+
+embedder = FaceNet()
+
+# load image from file
+image = Image.open("img/daario.jpg")
+image = image.convert("RGB")
+print(image)
+
+pixels = asarray(image)
+
+# Gets a detection dict for each face
+# in an image. Each one has the bounding box and
+# face landmarks (from mtcnn.MTCNN) along with
+# the embedding from FaceNet.
+detections = embedder.extract(pixels, threshold=0.95)
+
+print(detections)
