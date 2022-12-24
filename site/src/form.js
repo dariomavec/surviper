ea94c28fc2c695857be9e53976f035cb6ea05f19
dariomavec <dario.mavec@gmail.com>
2022-12-24 22:32:52 +1100

    Test deploy

--- site/src/form.js
+++ site/src/form.js
@@ -1,33 +1,18 @@
-function SubForm () {
-  // appendValues('1IaU8E938msrbuMN38qyjLyKuyRMMI1a3srSFtgLN96s', "Form!A1:D1")
-    // $.ajax({
-    //     url:'https://api.apispreadsheets.com/data/410/',
-    //     type:'post',
-    //     data:$("#myForm").serializeArray(),
-    //     success: function(){
-    //       alert("Form Data Submitted :)")
-    //     },
-    //     error: function(){
-    //       alert("There was an error :(")
-    //     }
-    // });
-}
+// const handleSubmit = (event) => {
+//   event.preventDefault();
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  const myForm = event.target;
-  const formData = new FormData(myForm);
+//   const myForm = event.target;
+//   const formData = new FormData(myForm);
   
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => console.log("Form successfully submitted"))
-    .catch((error) => alert(error));
-};
+//   fetch("/", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//     body: new URLSearchParams(formData).toString(),
+//   })
+//     .then(() => console.log("Form successfully submitted"))
+//     .catch((error) => alert(error));
+// };
 
-document
-  .querySelector("form")
-  .addEventListener("submit", handleSubmit);
+// document
+//   .querySelector("form")
+//   .addEventListener("submit", handleSubmit);
